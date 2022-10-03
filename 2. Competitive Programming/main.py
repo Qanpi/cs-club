d2034ae1e25884911f4feda861f09b389f50c3c5
Qanpi <aleksei.terin@outlook.com>
2022-10-03 17:58:15 +0300

    session 2 material

---
+++ 2. Competitive Programming/main.py
@@ -0,0 +1,15 @@
+text = "How could we count how many times I said 'how' in this sentence we?"
+
+words = text.split(" ")
+frequencies = {}
+
+for w in words:
+    w = w.lower()
+    w = w.replace("?", "")
+    w = w.replace("'", "")
+    if w in frequencies:
+        frequencies[w] += 1
+    else: 
+        frequencies[w] = 1
+
+print(frequencies)
