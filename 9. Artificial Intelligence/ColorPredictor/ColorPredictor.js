7d6f68358d1b1af03a1e7bf8f983c057a8d21adf
Qanpi <61239034+Qanpi@users.noreply.github.com>
2023-03-07 12:04:07 +0200

    updated from what we've done during club

--- 9. Artificial Intelligence/ColorPredictor/ColorPredictor.js
+++ 9. Artificial Intelligence/ColorPredictor/ColorPredictor.js
@@ -1,86 +1,79 @@
-let black, white;
+let nn;
 
 function setup() {
-  createCanvas(1080, 720);
+  createCanvas(400, 300);
   noLoop();
   
-  black = {
-    x: width/2 - width/4, 
-    y: height/2 - 64,
-    text: "black"
+  nn = new NeuralNetwork(4);
+  
+  for (let i=0; i<100000; i++) {
+    train();
   }
   
-  white = {
-    x: width/2 + width/4,
-    y: height/2 - 64,
-    text: "white"
-  }
-
-  mousePressed();
+  mousePressed( );
 }
 
-function setRandomBgColor() {
-    let r = random(255);
-    let g = random(255);
-    let b = random(255);
-    background(r, g, b);
-    
-    return {r: r, g: g, b: b};
-}
-
-function showInstructions() {
-  textSize(64);
-  noStroke();
-  textAlign(CENTER, CENTER);
-  fill(0);
-  text(black.text, black.x, black.y);
-  fill(255);
-  text(white.text, white.x, white.y);
-  
-  fill(255); 
-  strokeWeight(3);
-  stroke(0);
-  textSize(16);
-  textAlign(LEFT, BOTTOM);
-  text("press 's' to swap font color; 'enter' to keep", 0, height); 
-}
-
-function showPick(foregroundColor) {
-  noStroke();
-  if (foregroundColor == 0) {
-    fill(0);
-    ellipse(black.x, black.y + 100, 50);  
-  } else {
-    fill(255);
-    ellipse(white.x, white.y + 100, 50);
-  }
-}
-
-function predictForegroundColor(r, g, b) {
-  //if (r + g + b < 500) {return 255}
-  //else {return 0}
+function train() {
+  let r = random(255);
+  let g = random(255);
+  let b = random(255);
+  let target = predictColor(r, g, b);
+  nn.train([r, g, b, 200], target);
 }
 
 function mousePressed() {
-  let {r, g, b} = setRandomBgColor();
+  let r = random(255);
+  let g = random(255);
+  let b = random(255);
   
-  foregroundColor = predictForegroundColor(r, g, b);
-  showInstructions();
-  showPick(foregroundColor);  
+  background(r, g, b);
+  drawText();
+  
+  let guessedColor = nn.guess([r, g, b, 200]);
+  
+  let correctColor = predictColor(r, g, b);
+  
+  drawPick(correctColor, 50);
+  drawPick(guessedColor, 30); 
+}
 
-  if (mouseX > width/2) {
-    
+function drawPick(color, size) {
+  if (color == 1) {
+    fill(255);
+  } else if (color == -1) {
+    fill(0);
+  }
+  
+  ellipse(width/2, height/2, size);
+}
+
+function predictColor(r, g, b) {
+  let threshold = 300;
+
+  if (r < 100 && g < 200 && b < 100) {
+    return 1; //white
+  } else {
+    return -1; //black
   }
 }
 
+function predictColorTraining(r) {
+  let threshold = 200;
 
-
-function keyPressed() {
-  if (key == "s") {
-    
+  if (r < threshold) {
+    return 1; //white
+  } else {
+    return -1; //black
   }
 }
 
-function draw() {
+function drawText() {
+  textSize(64);
+  noStroke();
+  fill(0);
+  textAlign(CENTER, CENTER);
+  text("black", width/2 - width/4, height/2);
   
+  fill(255);
+  text("white", width/2 + width/4, height/2);
 }
